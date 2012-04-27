5f3ccb9c8875736d320b2dd1f9cfa439f62017f7
Andrew Crump <andrew@kotirisoftware.com>
2012-04-27 14:41:45 +0100

    Remove old scaffolding.

--- lib/test-kitchen/scaffold.rb
+++ lib/test-kitchen/scaffold.rb
@@ -9,52 +9,19 @@ module TestKitchen
       scaffold_file '.gitignore',
         <<-eos
           .bundle
+          .cache
+          .kitchen
           bin
         eos
 
       scaffold_file 'Gemfile',
         <<-eos
           gem 'test-kitchen'
-
-          # Add your other dependencies below:
-          # gem 'cucumber'
-          # gem 'librarian'
         eos
 
-      scaffold_file 'Vagrantfile',
+      scaffold_file 'test/setup/README.md',
         <<-eos
-          require 'test-kitchen'
-          TestKitchen.setup
-        eos
-
-      scaffold_file 'config/projects.json',
-        <<-eos
-          {
-            "projects": {
-              "foo": {
-                "language": "ruby",
-                "rvm": ["1.9.2"],
-                "repository": "https://github.com/you/your-repo.git",
-                "revision": "master",
-                "script": "bundle exec rspec spec",
-                "memory": "256"
-              }
-            }
-          }
-        eos
-
-      scaffold_file 'cookbooks/README.md',
-        <<-eos
-          Place any cookbooks required for your testing here.
-
-          You can add recipes to the run_list by adding the following to your
-          `Vagrantfile`.
-
-              TestKitchen::Vagrant.configure :vagrant do |tk_config, vm_config|
-                vm_config.vm.provision :chef_solo do |chef|
-                  chef.add_recipe 'foo::default'
-                end
-              end
+          Place any cookbooks required for your testing under here.
         eos
     end
 
