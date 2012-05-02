c2f9cf09bfa5942cedb00d2132392cec851dd1ae
Andrew Crump <andrew@kotirisoftware.com>
2012-05-02 15:56:52 +0100

    Override commands when testing cookbooks.

--- lib/test-kitchen/project/cookbook.rb
+++ lib/test-kitchen/project/cookbook.rb
@@ -7,6 +7,29 @@ module TestKitchen
       def lint(arg=nil)
         set_or_return(:lint, arg, {:default => true})
       end
+
+      def language(arg=nil)
+        "chef"
+      end
+
+      def install_command(runtime=nil)
+        "sudo gem update --system; gem install bundler && #{cd} && #{path} bundle install"
+      end
+
+      def test_command(runtime=nil)
+        "#{cd} && #{path} bundle exec cucumber -t @#{name} features"
+      end
+
+      private
+
+      def cd
+        "cd #{File.join(guest_test_root, 'test')}"
+      end
+
+      def path
+        'PATH=$PATH:/var/lib/gems/1.8/bin'
+      end
+
     end
   end
 end
