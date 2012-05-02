8ee6d6ae23fbd97baa0d1171c02fdc68005613ed
Andrew Crump <andrew@kotirisoftware.com>
2012-05-02 17:45:44 +0100

    Add code coverage calculation.

--- Gemfile
+++ Gemfile
@@ -10,4 +10,5 @@ group :development do
   gem 'cucumber', '~> 1.1.8'
   gem 'minitest', '~> 2.12.1'
   gem "rake", '~> 0.9.2.2'
+  gem 'simplecov', '~> 0.6.1'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'minitest/spec'
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require_relative '../lib/test-kitchen'
