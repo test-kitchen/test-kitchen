09bde93ef59694e08d7dfd0564ee6f11e4aeb95b
Andrew Crump <andrew@kotirisoftware.com>
2012-05-07 09:19:51 +0100

    Fail the build if coverage falls below threshold.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,12 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'minitest/spec'
 
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/'
+SimpleCov.at_exit do
+  SimpleCov.result.format!
+  if SimpleCov.result.covered_percent < 66
+    warn "Coverage is slipping: #{SimpleCov.result.covered_percent.to_i}%"
+    exit 1
+  end
 end
 
 require_relative '../lib/test-kitchen'
