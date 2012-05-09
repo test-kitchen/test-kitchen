e17ed479c11cfa7faa0e6578d3d347df67154210
Seth Chisamore <schisamo@opscode.com>
2012-05-09 14:37:06 -0400

    simplify vagrant runner based on recent changes

--- lib/test-kitchen/runner/vagrant.rb
+++ lib/test-kitchen/runner/vagrant.rb
@@ -16,21 +16,15 @@
 # limitations under the License.
 #
 
-require 'forwardable'
 require 'vagrant'
-require 'vagrant/command/base'
 
 module TestKitchen
   module Runner
     class Vagrant < Base
-      extend ::Forwardable
 
-      def_delegator :@command_base, :with_target_vms
-
-      def initialize(env, opts={})
+      def initialize(env, options={})
         super
-        # TODO: We shouldn't do IO in the constructor
-        @command_base = ::Vagrant::Command::Base.new(nil, vagrant_env)
+        @env, @options = env, options
       end
 
       def provision
@@ -51,6 +45,7 @@ module TestKitchen
       end
 
       def execute_remote_command(vm, command, message=nil)
+        vm = vagrant_env.vms[vm.to_sym] unless vm.kind_of?(::Vagrant::VM)
         vm.ui.info(message, :color => :yellow) if message
         vm.channel.execute(command, :error_check => false) do |type, data|
           next if data =~ /stdin: is not a tty/
@@ -66,7 +61,6 @@ module TestKitchen
 
       def vagrant_env
         @vagrant_env ||= begin
-
           env.create_tmp_file('Vagrantfile',
             IO.read(TestKitchen.source_root.join('config', 'Vagrantfile')))
 
@@ -74,6 +68,7 @@ module TestKitchen
             :ui_class => ::Vagrant::UI::Colored,
             :cwd => env.tmp_path
           }
+
           env = ::Vagrant::Environment.new(options)
           env.load!
           env
