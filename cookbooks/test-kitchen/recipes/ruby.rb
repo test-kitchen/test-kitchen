83f7df869aad3340c3387ac2a940aa8fb13fc1e6
Andrew Crump <andrew@kotirisoftware.com>
2012-05-18 14:28:32 +0100

    Assume rvm is present on the base image.

--- cookbooks/test-kitchen/recipes/ruby.rb
+++ cookbooks/test-kitchen/recipes/ruby.rb
@@ -18,10 +18,6 @@
 
 project = node.run_state[:project]
 
-node['rvm']['user_installs'] = [
-  { 'user' => node['test-kitchen']['user'] }
-]
-
 gemfile_path = File.join(project['test_root'], 'Gemfile')
 
 # TODO - remove this when test-kitchen is public
@@ -38,37 +34,4 @@ end
 # ensure we blow away Gemfile.lock
 file "#{gemfile_path}.lock" do
   action :delete
-end
-
-include_recipe "rvm::user_install"
-
-project['runtimes'].each do |runtime|
-
-  rvm_ruby runtime do
-    user node['test-kitchen']['user']
-    action :install
-  end
-
-  # default gems
-  %w{ bundler rake }.each do |gem|
-    rvm_gem gem do
-      ruby_string runtime
-      user node['test-kitchen']['user']
-      action :install
-    end
-  end
-
-  rvm_shell "[#{runtime}] bundle for [#{project['test_root']}]" do
-    ruby_string runtime
-    user node['test-kitchen']['user']
-    group node['test-kitchen']['group']
-    cwd project['test_root']
-    code project['install']
-  end
-
-end
-
-rvm_default_ruby '1.9.2' do
-  user node['test-kitchen']['user']
-  action :create
 end
