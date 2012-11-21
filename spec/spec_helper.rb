ecfc8a42bebbd94b50c69ba676c04d7a3227bbdd
jtimberman <joshua@opscode.com>
2012-11-20 17:44:56 -0700

    [KITCHEN-5] - remove exit 1 from simplecov check

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,6 @@ SimpleCov.at_exit do
   SimpleCov.result.format!
   if SimpleCov.result.covered_percent < 66
     warn "Coverage is slipping: #{SimpleCov.result.covered_percent.to_i}%"
-    exit 1
   end
 end
 
