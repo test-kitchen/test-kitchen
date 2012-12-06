602166ca6ebed97c3beb0a53a3bfde0ac96f2212
Matt Ray <matthewhray@gmail.com>
2012-12-05 20:54:03 -0600

    add floating_ip parameter

--- lib/test-kitchen/runner/openstack/dsl.rb
+++ lib/test-kitchen/runner/openstack/dsl.rb
@@ -4,7 +4,7 @@ module TestKitchen
   class Openstack
     include Chef::Mixin::ParamsValidate
 
-    attr_writer :username, :password, :tenant, :auth_url
+    attr_writer :username, :password, :tenant, :auth_url, :floating_ip
 
     def initialize(&block)
       instance_eval(&block) if block_given?
@@ -24,6 +24,10 @@ module TestKitchen
 
     def auth_url(arg=nil)
       set_or_return(:auth_url, arg, {})
+    end
+
+    def floating_ip(arg=nil)
+      set_or_return(:floating_ip, arg, {})
     end
   end
 end
