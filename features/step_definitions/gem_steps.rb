e6472e762394875d7d5860038581696da94ddf29
Fletcher Nichol <fnichol@nichol.ca>
2013-03-21 15:58:57 -0600

    Add flag to `kitchen init` to skip Gemfile creation by default.
    This adds a `--create-gemfile` flag (and its reverse of
    `--no-create-gemfile`) to `kitchen init` with a default false value. As
    discussed with @schisamo, this should help in situations where Test
    Kitchen has been installed as a gem and not necessarily brought in using
    Bundler. The updated behavior is as follows:
    
    * If a Gemfile exists, assume we want to use that and append gems to the
      Gemfile. Display a message instructing the user to `bundle install`.
    * If no Gemfile exists, issue `gem install` commands to install the
      drivers. The default value of --create-gemfile is false.
    * If --create-gemfile flag is present (value of true), create and append
      to a Gemfile. Display a message instructing the user to
      `bundle install`.

---
+++ features/step_definitions/gem_steps.rb
@@ -0,0 +1,24 @@
+require 'tmpdir'
+require 'pathname'
+
+Given(/^a sandboxed GEM_HOME directory named "(.*?)"$/) do |name|
+  backup_envvar('GEM_HOME')
+  backup_envvar('GEM_PATH')
+
+  gem_home = Pathname.new(Dir.mktmpdir(name))
+  ENV['GEM_HOME'] = gem_home.to_s
+  ENV['GEM_PATH'] = [gem_home.to_s, ENV['GEM_PATH']].join(':')
+  @cleanup_dirs << gem_home
+end
+
+Then(/^a gem named "(.*?)" is installed with version "(.*?)"$/) do |name, version|
+  unbundlerize do
+    run_simple(unescape("gem list #{name} --version #{version} -i"), true, nil)
+  end
+end
+
+Then(/^a gem named "(.*?)" is installed$/) do |name|
+  unbundlerize do
+    run_simple(unescape("gem list #{name} -i"), true, nil)
+  end
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,8 @@ require 'aruba/cucumber'
 require 'kitchen'
 
 Before do
-  @aruba_timeout_seconds = 5
+  @aruba_timeout_seconds = 15
+  @cleanup_dirs = []
 end
 
 After do |s|
@@ -11,4 +12,28 @@ After do |s|
   # This is useful to inspect the 'tmp/aruba' directory before any other
   # steps are executed and clear it out.
   Cucumber.wants_to_quit = true if s.failed?
+
+  # Restore environment variables to their original settings, if they have
+  # been saved off
+  ENV.keys.select { |key| key =~ /^_CUKE_/ }.each do |backup_key|
+    ENV[backup_key.sub(/^_CUKE_/, '')] = ENV.delete(backup_key)
+  end
+
+  @cleanup_dirs.each { |dir| FileUtils.rm_rf(dir) }
+end
+
+def backup_envvar(key)
+  ENV["_CUKE_#{key}"] = ENV[key]
+end
+
+def restore_envvar(key)
+  ENV[key] = ENV.delete("_CUKE_#{key}")
+end
+
+def unbundlerize
+  keys = %w[BUNDLER_EDITOR BUNDLE_BIN_PATH BUNDLE_GEMFILE RUBYOPT]
+
+  keys.each { |key| backup_envvar(key) ; ENV.delete(key) }
+  yield
+  keys.each { |key| restore_envvar(key) }
 end
