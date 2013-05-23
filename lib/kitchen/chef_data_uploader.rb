66e401357868cbb9f52f52c0f86d484b017e0be5
Fletcher Nichol <fnichol@nichol.ca>
2013-05-22 23:34:38 -0600

    Raise a UserError if the name attribute isn't in metadata.rb.
    References #116

--- lib/kitchen/chef_data_uploader.rb
+++ lib/kitchen/chef_data_uploader.rb
@@ -162,9 +162,9 @@ module Kitchen
 
     def cp_this_cookbook(tmpdir)
       metadata_rb = File.join(kitchen_root, "metadata.rb")
-      cb_name = MetadataChopper.extract(metadata_rb).first
-
-      raise TestKitchen::Errors::MissingCookbookName.new(name) if cb_name.nil?
+      cb_name = MetadataChopper.extract(metadata_rb).first or raise(UserError,
+        "The metadata.rb does not define the 'name' key." +
+          " Please add: `name '<cookbook_name>'` to metadata.rb and try again")
 
       cb_path = File.join(tmpdir, cb_name)
       glob = Dir.glob("#{kitchen_root}/{metadata.rb,README.*," +
