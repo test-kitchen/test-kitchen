9c986f51f64c198f3874b2fe7cb97c9309a237de
Fletcher Nichol <fnichol@nichol.ca>
2013-11-20 08:43:18 -0700

    Revert "Make Ruby binpath part of the remote PATH if it's configured"
    This reverts commit 858a1f134001e877b0b54769cab3fede5b132886.

--- lib/kitchen/driver/ssh_base.rb
+++ lib/kitchen/driver/ssh_base.rb
@@ -104,11 +104,7 @@ module Kitchen
         env = "env"
         env << " http_proxy=#{config[:http_proxy]}"   if config[:http_proxy]
         env << " https_proxy=#{config[:https_proxy]}" if config[:https_proxy]
-
-        additional_paths = []
-        additional_paths << config[:ruby_binpath] if config[:ruby_binpath]
-        additional_paths << config[:path] if config[:path]
-        env << " PATH=$PATH:#{additional_paths.join(':')}" if additional_paths.any?
+        env << " PATH=#{config[:path]}" if config[:path]
 
         env == "env" ? cmd : "#{env} #{cmd}"
       end
