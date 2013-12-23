1a396c00687d715d16eca8b6531263b039afe5fb
Fletcher Nichol <fnichol@nichol.ca>
2013-12-23 07:42:23 -0700

    Extract & refactor `kitchen console` subcommand.

--- lib/kitchen/cli.rb
+++ lib/kitchen/cli.rb
@@ -203,12 +203,7 @@ module Kitchen
 
     desc "console", "Kitchen Console!"
     def console
-      require 'pry'
-      Pry.start(@config, :prompt => pry_prompts)
-    rescue LoadError => e
-      warn %{Make sure you have the pry gem installed. You can install it with:}
-      warn %{`gem install pry` or including 'gem "pry"' in your Gemfile.}
-      exit 1
+      perform("console", "console")
     end
 
     register Kitchen::Generator::Init, "init",
@@ -416,23 +411,6 @@ module Kitchen
         banner "Invoking init as '#{yaml}' file is missing"
         invoke "init"
       end
-    end
-
-    def pry_prompts
-      [
-        proc { |target_self, nest_level, pry|
-          ["[#{pry.input_array.size}] ",
-            "kc(#{Pry.view_clip(target_self.class)})",
-            "#{":#{nest_level}" unless nest_level.zero?}> "
-          ].join
-        },
-        proc { |target_self, nest_level, pry|
-          ["[#{pry.input_array.size}] ",
-            "kc(#{Pry.view_clip(target_self.class)})",
-            "#{":#{nest_level}" unless nest_level.zero?}* "
-          ].join
-        },
-      ]
     end
   end
 end
---
+++ lib/kitchen/command/console.rb
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Fletcher Nichol (<fnichol@nichol.ca>)
+#
+# Copyright (C) 2013, Fletcher Nichol
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'kitchen/command'
+
+module Kitchen
+
+  module Command
+
+    # Command to launch a Pry-based Kitchen console..
+    #
+    # @author Fletcher Nichol <fnichol@nichol.ca>
+    class Console < Kitchen::Command::Base
+
+      def call
+        require 'pry'
+        Pry.start(@config, :prompt => pry_prompts)
+      rescue LoadError => e
+        warn %{Make sure you have the pry gem installed. You can install it with:}
+        warn %{`gem install pry` or including 'gem "pry"' in your Gemfile.}
+        exit 1
+      end
+
+      protected
+
+      def pry_prompts
+        [
+          proc { |target_self, nest_level, pry|
+            ["[#{pry.input_array.size}] ",
+              "kc(#{Pry.view_clip(target_self.class)})",
+              "#{":#{nest_level}" unless nest_level.zero?}> "
+            ].join
+          },
+          proc { |target_self, nest_level, pry|
+            ["[#{pry.input_array.size}] ",
+              "kc(#{Pry.view_clip(target_self.class)})",
+              "#{":#{nest_level}" unless nest_level.zero?}* "
+            ].join
+          },
+        ]
+      end
+    end
+  end
+end
