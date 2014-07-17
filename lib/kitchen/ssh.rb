0feb3af43f9870936f44172f3cf7d7014fc8b4d2
Fletcher Nichol <fnichol@nichol.ca>
2014-07-17 12:29:44 -0600

    Complete spec and yard docs coverage for SSH, y'all have no idea!
    This one was huge to get done, huge I tells ya!

--- lib/kitchen/ssh.rb
+++ lib/kitchen/ssh.rb
@@ -37,6 +37,29 @@ module Kitchen
   # @author Fletcher Nichol <fnichol@nichol.ca>
   class SSH
 
+    # Constructs a new SSH object.
+    #
+    # @example basic usage
+    #
+    #   ssh = Kitchen::SSH.new("remote.example.com", "root")
+    #   ssh.exec("sudo apt-get update")
+    #   ssh.upload!("/tmp/data.txt", "/var/lib/data.txt")
+    #   ssh.shutdown
+    #
+    # @example block usage
+    #
+    #   Kitchen::SSH.new("remote.example.com", "root") do |ssh|
+    #     ssh.exec("sudo apt-get update")
+    #     ssh.upload!("/tmp/data.txt", "/var/lib/data.txt")
+    #   end
+    #
+    # @param hostname [String] the remote hostname (IP address, FQDN, etc.)
+    # @param username [String] the username for the remote host
+    # @param options [Hash] configuration options
+    # @option options [Logger] :logger the logger to use
+    #   (default: `::Logger.new(STDOUT)`)
+    # @yield [self] if a block is given then the constructed object yields
+    #   itself and calls `#shutdown` at the end, closing the remote connection
     def initialize(hostname, username, options = {})
       @hostname = hostname
       @username = username
@@ -49,6 +72,10 @@ module Kitchen
       end
     end
 
+    # Execute a command on the remote host.
+    #
+    # @param cmd [String] command string to execute
+    # @raise [SSHFailed] if the command does not exit with a 0 code
     def exec(cmd)
       logger.debug("[SSH] #{self} (#{cmd})")
       exit_code = exec_with_exit(cmd)
@@ -58,6 +85,13 @@ module Kitchen
       end
     end
 
+    # Uploads a local file to remote host.
+    #
+    # @param local [String] path to local file
+    # @param remote [String] path to remote file destination
+    # @param options [Hash] configuration options that are passed to
+    #   `Net::SCP.upload`
+    # @see http://net-ssh.github.io/net-scp/classes/Net/SCP.html#method-i-upload
     def upload!(local, remote, options = {}, &progress)
       if progress.nil?
         progress = lambda { |ch, name, sent, total|
@@ -70,12 +104,21 @@ module Kitchen
       session.scp.upload!(local, remote, options, &progress)
     end
 
+    # Uploads a recursive directory to remote host.
+    #
+    # @param local [String] path to local file or directory
+    # @param remote [String] path to remote file destination
+    # @param options [Hash] configuration options that are passed to
+    #   `Net::SCP.upload`
+    # @option options [true,false] :recursive recursive copy (default: `true`)
+    # @see http://net-ssh.github.io/net-scp/classes/Net/SCP.html#method-i-upload
     def upload_path!(local, remote, options = {}, &progress)
       options = { :recursive => true }.merge(options)
 
       upload!(local, remote, options, &progress)
     end
 
+    # Shuts down the session connection, if it is still active.
     def shutdown
       return if @session.nil?
 
@@ -85,10 +128,15 @@ module Kitchen
       @session = nil
     end
 
+    # Blocks until the remote host's SSH TCP port is listening.
     def wait
       logger.info("Waiting for #{hostname}:#{port}...") until test_ssh
     end
 
+    # Builds a LoginCommand which can be used to open an interactive session
+    # on the remote host.
+    #
+    # @return [LoginCommand] the login command
     def login_command
       args  = %W{ -o UserKnownHostsFile=/dev/null }
       args += %W{ -o StrictHostKeyChecking=no }
@@ -104,12 +152,34 @@ module Kitchen
 
     private
 
-    attr_reader :hostname, :username, :options, :logger
+    # @return [String] the remote hostname
+    # @api private
+    attr_reader :hostname
 
+    # @return [String] the username for the remote host
+    # @api private
+    attr_reader :username
+
+    # @return [Hash] SSH options, passed to `Net::SSH.start`
+    attr_reader :options
+
+    # @return [Logger] the logger to use
+    # @api private
+    attr_reader :logger
+
+    # Builds the Net::SSH session connection or returns the existing one if
+    # built.
+    #
+    # @return [Net::SSH::Connection::Session] the SSH connection session
+    # @api private
     def session
       @session ||= establish_connection
     end
 
+    # Establish a connection session to the remote host.
+    #
+    # @return [Net::SSH::Connection::Session] the SSH connection session
+    # @api private
     def establish_connection
       rescue_exceptions = [
         Errno::EACCES, Errno::EADDRINUSE, Errno::ECONNREFUSED,
@@ -133,14 +203,25 @@ module Kitchen
       end
     end
 
+    # String representation of object, reporting its connection details and
+    # configuration.
+    #
+    # @api private
     def to_s
       "#{username}@#{hostname}:#{port}<#{options.inspect}>"
     end
 
+    # @return [Integer] SSH port (default: 22)
+    # @api private
     def port
       options.fetch(:port, 22)
     end
 
+    # Execute a remote command and return the command's exit code.
+    #
+    # @param cmd [String] command string to execute
+    # @return [Integer] the exit code of the command
+    # @api private
     def exec_with_exit(cmd)
       exit_code = nil
       session.open_channel do |channel|
@@ -166,6 +247,11 @@ module Kitchen
       exit_code
     end
 
+    # Test a remote TCP socket (presumably SSH) for connectivity.
+    #
+    # @return [true,false] a truthy value if the socket is ready and false
+    #   otherwise
+    # @api private
     def test_ssh
       socket = TCPSocket.new(hostname, port)
       IO.select([socket], nil, nil, 5)
