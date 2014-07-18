b9e2a3d909d8c46d17dc649005d802248f80834b
Fletcher Nichol <fnichol@nichol.ca>
2014-07-17 23:08:20 -0600

    Complete yard docs coverage for Provisioner::Chef::Berkshelf.

--- lib/kitchen/provisioner/chef/berkshelf.rb
+++ lib/kitchen/provisioner/chef/berkshelf.rb
@@ -74,11 +74,23 @@ module Kitchen
 
         private
 
-        attr_reader :berksfile, :path, :logger
+        # @return [String] path to a Berksfile
+        # @api private
+        attr_reader :berksfile
+
+        # @return [String] path in which to vendor the resulting cookbooks
+        # @api private
+        attr_reader :path
+
+        # @return [Kitchen::Logger] a logger to use for output
+        # @api private
+        attr_reader :logger
 
         # Load the Berkshelf-specific libary code.
         #
         # @param logger [Kitchen::Logger] the logger to use
+        # @raise [UserError] if the library couldn't be loaded
+        # @api private
         def self.load_berkshelf!(logger)
           first_load = require 'berkshelf'
 
