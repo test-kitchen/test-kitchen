db198342312855cda15ee57039037d59dae8eae9
Jacob McCann <jacob.mccann2@target.com>
2014-09-15 09:24:56 -0500

    Fix to work with http chef_omnibus_url
    Often people will reference getchef.com with http rather then https
    for either using proxy caching software or due to The Enterprise
    SSL proxy.  This allows the chef_omnibus_url to properly be updated
    from .sh to .msi when referenced with http and not https.

--- lib/kitchen/provisioner/chef_base.rb
+++ lib/kitchen/provisioner/chef_base.rb
@@ -196,7 +196,7 @@ module Kitchen
           install_flags = %w[latest true].include?(version) ? "" : "v=#{version}"
 
           # If we have the default URL for UNIX then we change it for the Windows version.
-          if config[:chef_omnibus_url].eql?("https://www.getchef.com/chef/install.sh")
+          if config[:chef_omnibus_url] =~ %r{http[s]*://www.getchef.com/chef/install.sh}
             chef_url = "http://www.getchef.com/chef/install.msi?#{install_flags}"
           else
             # We use the one that comes from kitchen.yml
