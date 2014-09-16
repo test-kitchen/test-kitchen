657e14324325dab4c452e6a3999b56d455becb49
Chris Aumann <me@chr4.org>
2014-09-16 13:31:12 +0200

    Use single quotes in Thorfile templates
    This fixes issues when running rubocop. See https://github.com/berkshelf/berkshelf/pull/1301

--- lib/kitchen/generator/init.rb
+++ lib/kitchen/generator/init.rb
@@ -135,10 +135,10 @@ module Kitchen
         rakedoc = <<-RAKE.gsub(/^ {10}/, "")
 
           begin
-            require "kitchen/rake_tasks"
+            require 'kitchen/rake_tasks'
             Kitchen::RakeTasks.new
           rescue LoadError
-            puts ">>>>> Kitchen gem not loaded, omitting tasks" unless ENV["CI"]
+            puts '>>>>> Kitchen gem not loaded, omitting tasks' unless ENV['CI']
           end
         RAKE
         append_to_file(File.join(destination_root, "Rakefile"), rakedoc)
@@ -153,10 +153,10 @@ module Kitchen
         thordoc = <<-THOR.gsub(/^ {10}/, "")
 
           begin
-            require "kitchen/thor_tasks"
+            require 'kitchen/thor_tasks'
             Kitchen::ThorTasks.new
           rescue LoadError
-            puts ">>>>> Kitchen gem not loaded, omitting tasks" unless ENV["CI"]
+            puts '>>>>> Kitchen gem not loaded, omitting tasks' unless ENV['CI']
           end
         THOR
         append_to_file(File.join(destination_root, "Thorfile"), thordoc)
