d3cad638399f17e0e69f78068cc30d2cee04386d
Matt Wrock <matt@mattwrock.com>
2015-01-04 12:49:02 -0800

    starting to address busser tests previously commented out

--- spec/kitchen/busser_spec.rb
+++ spec/kitchen/busser_spec.rb
@@ -192,11 +192,6 @@ describe Kitchen::Busser do
         cmd.must_match regexify(%{GEM_CACHE="/r/gems/cache"}, :partial_line)
       end
 
-      # it "exports all the environment variables" do
-      #   cmd.must_match regexify(
-      #     "export BUSSER_ROOT GEM_HOME GEM_PATH GEM_CACHE", :partial_line)
-      # end
-
       it "checks if busser is installed" do
         cmd.must_match regexify(
           %{if ! sudo -E /r/gem list busser -i >/dev/null;}, :partial_line)
@@ -256,169 +251,142 @@ describe Kitchen::Busser do
     end
   end
 
-  # describe "#sync_cmd" do
+  describe "#cleanup_cmd" do
 
-  #   before do
-  #     @root = Dir.mktmpdir
-  #     config[:test_base_path] = @root
-  #   end
+    before do
+      @root = Dir.mktmpdir
+      config[:test_base_path] = @root
+    end
 
-  #   after do
-  #     FileUtils.remove_entry(@root)
-  #   end
+    after do
+      FileUtils.remove_entry(@root)
+    end
 
-  #   let(:cmd) { busser.sync_cmd }
+    let(:cmd) { busser.cleanup_cmd }
 
-  #   describe "with no suite test files" do
+    describe "with no suite test files" do
 
-  #     it "returns nil" do
-  #       cmd.must_equal nil
-  #     end
-  #   end
+      it "returns nil" do
+        cmd.must_equal nil
+      end
+    end
 
-  #   describe "with suite test files" do
+    describe "with suite test files" do
 
-  #     before do
-  #       base = "#{config[:test_base_path]}/germany"
-  #       hbase = "#{config[:test_base_path]}/helpers"
+      before do
+        base = "#{config[:test_base_path]}/germany"
 
-  #       files.map { |f, md| [File.join(base, f), md] }.each do |f, md|
-  #         create_file(f, md[:content], md[:perms])
-  #       end
-  #       helper_files.map { |f, md| [File.join(hbase, f), md] }.each do |f, md|
-  #         create_file(f, md[:content], md[:perms])
-  #       end
+        files.map { |f, md| [File.join(base, f), md] }.each do |f, md|
+          create_file(f, md[:content], md[:perms])
+        end
 
-  #       config[:ruby_bindir] = "/r"
-  #     end
+        config[:ruby_bindir] = "/r"
+      end
 
-  #     let(:files) do
-  #       {
-  #         "mondospec/charlie" => {
-  #           :content => "charlie",
-  #           :perms => "0764",
-  #           :base64 => "Y2hhcmxpZQ==",
-  #           :md5 => "bf779e0933a882808585d19455cd7937"
-  #         },
-  #         "minispec/beta" => {
-  #           :content => "beta",
-  #           :perms => "0644",
-  #           :base64 => "YmV0YQ==",
-  #           :md5 => "987bcab01b929eb2c07877b224215c92"
-  #         },
-  #         "abba/alpha" => {
-  #           :content => "alpha",
-  #           :perms => "0440",
-  #           :base64 => "YWxwaGE=",
-  #           :md5 => "2c1743a391305fbf367df8e4f069f9f9"
-  #         }
-  #       }
-  #     end
+      let(:files) do
+        {
+          "abba/alpha" => {
+            :content => "alpha",
+            :perms => "0440",
+            :base64 => "YWxwaGE=",
+            :md5 => "2c1743a391305fbf367df8e4f069f9f9"
+          }
+        }
+      end
 
-  #     let(:helper_files) do
-  #       {
-  #         "minispec/spec_helper" => {
-  #           :content => "helping",
-  #           :perms => "0644",
-  #           :base64 => "aGVscGluZw==",
-  #           :md5 => "111c081293c11cb7c2ac6fbf841805cb"
-  #         },
-  #         "abba/common" => {
-  #           :content => "yeppers",
-  #           :perms => "0664",
-  #           :base64 => "eWVwcGVycw==",
-  #           :md5 => "7c3157de4890b1abcb7a6a3695eb6dd2"
-  #         }
-  #       }
-  #     end
+      let(:helper_files) do
+        {
+          "abba/common" => {
+            :content => "yeppers",
+            :perms => "0664",
+            :base64 => "eWVwcGVycw==",
+            :md5 => "7c3157de4890b1abcb7a6a3695eb6dd2"
+          }
+        }
+      end
 
-  #     it "uses bourne shell" do
-  #       cmd.must_match(/\Ash -c '$/)
-  #       cmd.must_match(/'\Z/)
-  #     end
+      it "uses wraps command with shell" do
+        cmd.must_match(/\Ash -c '$/)
+        cmd.must_match(/'\Z/)
+      end
 
-  #     it "sets the BUSSER_ROOT environment variable" do
-  #       config[:root_path] = "/r"
+      it "sets the BUSSER_ROOT environment variable" do
+        config[:root_path] = "/r"
 
-  #       cmd.must_match regexify(%{BUSSER_ROOT="/r"}, :partial_line)
-  #     end
+        cmd.must_match regexify(%{BUSSER_ROOT="/r"})
+      end
 
-  #     it "sets the GEM_HOME environment variable" do
-  #       config[:root_path] = "/r"
+      it "sets the GEM_HOME environment variable" do
+        config[:root_path] = "/r"
 
-  #       cmd.must_match regexify(%{GEM_HOME="/r/gems" }, :partial_line)
-  #     end
+        cmd.must_match regexify(%{GEM_HOME="/r/gems"})
+      end
 
-  #     it "sets the GEM_PATH environment variable" do
-  #       config[:root_path] = "/r"
+      it "sets the GEM_PATH environment variable" do
+        config[:root_path] = "/r"
 
-  #       cmd.must_match regexify(%{GEM_PATH="/r/gems" }, :partial_line)
-  #     end
+        cmd.must_match regexify(%{GEM_PATH="/r/gems"})
+      end
 
-  #     it "sets the GEM_CACHE environment variable" do
-  #       config[:root_path] = "/r"
+      it "sets the GEM_CACHE environment variable" do
+        config[:root_path] = "/r"
 
-  #       cmd.must_match regexify(%{GEM_CACHE="/r/gems/cache" }, :partial_line)
-  #     end
+        cmd.must_match regexify(%{GEM_CACHE="/r/gems/cache"})
+      end
 
-  #     it "exports all the environment variables" do
-  #       cmd.must_match regexify(
-  #         "export BUSSER_ROOT GEM_HOME GEM_PATH GEM_CACHE", :partial_line)
-  #     end
+      # it "logs a message for each file" do
+      #   config[:busser_bin] = "/b/busser"
 
-  #     it "logs a message for each file" do
-  #       config[:busser_bin] = "/b/busser"
+      #   files.each do |f, md|
+      #     cmd.must_match regexify([
+      #       %{echo "Uploading `sudo -E /b/busser suite path`/#{f}},
+      #       %{(mode=#{md[:perms]})"}
+      #     ].join(" "))
+      #   end
+      # end
 
-  #       files.each do |f, md|
-  #         cmd.must_match regexify([
-  #           %{echo "Uploading `sudo -E /b/busser suite path`/#{f}},
-  #           %{(mode=#{md[:perms]})"}
-  #         ].join(" "))
-  #       end
-  #     end
+      # it "logs a message for each helper file" do
+      #   config[:busser_bin] = "/b/busser"
 
-  #     it "logs a message for each helper file" do
-  #       config[:busser_bin] = "/b/busser"
+      #   helper_files.each do |f, md|
+      #     cmd.must_match regexify([
+      #       %{echo "Uploading `sudo -E /b/busser suite path`/#{f}},
+      #       %{(mode=#{md[:perms]})"}
+      #     ].join(" "))
+      #   end
+      # end
 
-  #       helper_files.each do |f, md|
-  #         cmd.must_match regexify([
-  #           %{echo "Uploading `sudo -E /b/busser suite path`/#{f}},
-  #           %{(mode=#{md[:perms]})"}
-  #         ].join(" "))
-  #       end
-  #     end
+      # it "base64 encodes each file for deserializing with busser" do
+      #   config[:busser_bin] = "/b/busser"
 
-  #     it "base64 encodes each file for deserializing with busser" do
-  #       config[:busser_bin] = "/b/busser"
+      #   files.each do |f, md|
+      #     cmd.must_match regexify([
+      #       %{echo "#{md[:base64]}" | sudo -E /b/busser deserialize},
+      #       %{--destination=`sudo -E /b/busser suite path`/#{f}},
+      #       %{--md5sum=#{md[:md5]} --perms=#{md[:perms]}}
+      #     ].join(" "))
+      #   end
+      # end
 
-  #       files.each do |f, md|
-  #         cmd.must_match regexify([
-  #           %{echo "#{md[:base64]}" | sudo -E /b/busser deserialize},
-  #           %{--destination=`sudo -E /b/busser suite path`/#{f}},
-  #           %{--md5sum=#{md[:md5]} --perms=#{md[:perms]}}
-  #         ].join(" "))
-  #       end
-  #     end
+      # it "base64 encodes each helper file for deserializing with busser" do
+      #   config[:busser_bin] = "/b/busser"
 
-  #     it "base64 encodes each helper file for deserializing with busser" do
-  #       config[:busser_bin] = "/b/busser"
+      #   helper_files.each do |f, md|
+      #     cmd.must_match regexify([
+      #       %{echo "#{md[:base64]}" | sudo -E /b/busser deserialize},
+      #       %{--destination=`sudo -E /b/busser suite path`/#{f}},
+      #       %{--md5sum=#{md[:md5]} --perms=#{md[:perms]}}
+      #     ].join(" "))
+      #   end
+      # end
 
-  #       helper_files.each do |f, md|
-  #         cmd.must_match regexify([
-  #           %{echo "#{md[:base64]}" | sudo -E /b/busser deserialize},
-  #           %{--destination=`sudo -E /b/busser suite path`/#{f}},
-  #           %{--md5sum=#{md[:md5]} --perms=#{md[:perms]}}
-  #         ].join(" "))
-  #       end
-  #     end
-
-  #     def create_file(file, content, perms)
-  #       FileUtils.mkdir_p(File.dirname(file))
-  #       File.open(file, "wb") { |f| f.write(content) }
-  #       FileUtils.chmod(perms.to_i(8), file)
-  #     end
-  #   end
-  # end
+      def create_file(file, content, perms)
+        FileUtils.mkdir_p(File.dirname(file))
+        File.open(file, "wb") { |f| f.write(content) }
+        FileUtils.chmod(perms.to_i(8), file)
+      end
+    end
+  end
 
   describe "#run_cmd" do
 
@@ -483,11 +451,6 @@ describe Kitchen::Busser do
 
         cmd.must_match regexify(%{GEM_CACHE="/r/gems/cache"}, :partial_line)
       end
-
-      # it "exports all the environment variables" do
-      #   cmd.must_match regexify(
-      #     "export BUSSER_ROOT GEM_HOME GEM_PATH GEM_CACHE", :partial_line)
-      # end
 
       it "uses sudo for busser test when configured" do
         config[:sudo] = true
