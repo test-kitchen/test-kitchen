2989bfdf5f77fcaea7995f651e109ca898147065
Matt Wrock <matt@mattwrock.com>
2015-01-04 12:48:18 -0800

    starting to add tests for shells

---
+++ spec/kitchen/shell/bourne_spec.rb
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Matt Wrock (<matt@mattwrock.com>)
+#
+# Copyright (C) 2014, Matt Wrock
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative "../../spec_helper"
+
+require "kitchen"
+require "kitchen/shell/base"
+require "kitchen/shell/bourne"
+
+describe Kitchen::Shell::Bourne do
+
+  let(:shell) do
+    Kitchen::Shell::Bourne.new
+  end
+
+  describe "set_env" do
+
+    let(:env) { shell.set_env("my_var", "this is my value") }
+
+    it "sets the value given to the variable" do
+      env.must_equal(
+        <<-CMD.gsub(/^ {10}/, "")
+          my_var="this is my value"
+          export my_var
+        CMD
+      )
+    end
+  end
+
+  def regexify(str, line = :whole_line)
+    r = Regexp.escape(str)
+    r = "^\s*#{r}$" if line == :whole_line
+    Regexp.new(r)
+  end
+end
