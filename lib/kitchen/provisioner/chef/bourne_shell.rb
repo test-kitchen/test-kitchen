25ad983487680c6f8c5490bad753bfe50200cb2f
mwrock <matt@mattwrock.com>
2015-01-04 02:59:49 -0800

    add api comments to shell classes and modules

--- lib/kitchen/provisioner/chef/bourne_shell.rb
+++ lib/kitchen/provisioner/chef/bourne_shell.rb
@@ -1,35 +1,85 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Matt Wrock (<matt@mattwrock.com>)
+#
+# Copyright (C) 2014, Matt Wrock
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 module Kitchen
 
   module Provisioner
 
     module Chef
-      # blah
+
+      # Provides commands for interacting with chef on a test instance
+      #
+      # @author Matt Wrock <matt@mattwrock.com>
       module BourneShell
 
+        # The path to the root of a chef install
+        #
+        # @return [String] absolute path to chef root
         def chef_omnibus_root
           "/opt/chef"
         end
 
+        # path to chef-solo relative to the chef_omnibus_root
+        #
+        # @return [String] path to chef solo
         def chef_solo_file
           "bin/chef-solo"
         end
 
+        # path to chef-client relative to the chef_omnibus_root
+        #
+        # @return [String] path to chef client
         def chef_client_file
           "bin/chef-client"
         end
 
-        def init_command(config)
+        # A command for initializing an instance for working with chef
+        # the calling provisioner may have a omnibus root configured by
+        # the user that is different from this module's chef_omnibus_root.
+        #
+        # @param root_path [String] root path to chef omnibus
+        # @return [String] command to run on instance initializing
+        # chef environment
+        def init_command(root_path)
           dirs = %w[cookbooks data data_bags environments roles clients].
-            map { |dir| File.join(config[:root_path], dir) }.join(" ")
-          lines = ["#{sudo("rm")} -rf #{dirs}", "mkdir -p #{config[:root_path]}"]
+            map { |dir| File.join(root_path, dir) }.join(" ")
+          lines = ["#{sudo("rm")} -rf #{dirs}", "mkdir -p #{root_path}"]
 
           wrap_command([dirs, lines].join("\n"))
         end
 
+        # File name containing helper scripts for working with the chef
+        # environment on the test instance.
+        #
+        # @return [String] helper file name
         def chef_helper_file
           "chef_helpers.sh"
         end
 
+        # Install script for the chef client on the test instance.
+        #
+        # @param version [String] the chef version to install
+        # @param config [Hash] the calling provisioner's config
+        # @option opts [String] :chef_omnibus_install_options install
+        # options to pass to installer
+        # @option opts [String] :chef_omnibus_root path to the chef omnibus root
+        # @option opts [String] :chef_omnibus_url URL to download the chef installer
+        # @return [String] command to install chef
         def install_function(version, config)
           pretty_version = case version
                            when "true" then "install only if missing"
--- lib/kitchen/transport/winrm_file_transfer/remote_file.rb
+++ lib/kitchen/transport/winrm_file_transfer/remote_file.rb
@@ -1,3 +1,21 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Matt Wrock (<matt@mattwrock.com>)
+#
+# Copyright (C) 2014, Matt Wrock
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require "io/console"
 require "json"
 require "kitchen/transport/winrm_file_transfer/shell"
@@ -8,6 +26,8 @@ module Kitchen
 
       # Represents a file to be copied to the test instance
       # It maintains a winrm session to be used for all operations
+      #
+      # @author Matt Wrock <matt@mattwrock.com>
       class RemoteFile
 
         attr_reader :local_path
@@ -17,6 +37,14 @@ module Kitchen
         attr_reader :service
         attr_reader :shell
 
+        # Initializes a new remote file intended for uploading
+        # to the test instance
+        #
+        # @param logger [Logger] kitchen logger for messaging output
+        # @param service [WinRMWebService] an active winrm connection
+        # @param local_path [String] path to a file or directory to
+        # copy to the test instance
+        # @param remote_path [String] target path on test instance to copy to
         def initialize(logger, service, local_path, remote_path)
           @closed = false
           @service = service
