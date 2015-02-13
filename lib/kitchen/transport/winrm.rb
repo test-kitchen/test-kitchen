7f750241218c51ea4a26072846664dc8db514b7e
Fletcher Nichol <fnichol@nichol.ca>
2015-02-13 15:45:27 -0700

    Complete yard docs coverage for Transport::Winrm.

--- lib/kitchen/transport/winrm.rb
+++ lib/kitchen/transport/winrm.rb
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 #
 # Author:: Salim Afiune (<salim@afiunemaya.com.mx>)
+# Author:: Matt Wrock (<matt@mattwrock.com>)
+# Author:: Fletcher Nichol (<fnichol@nichol.ca>)
 #
 # Copyright (C) 2014, Salim Afiune
 #
@@ -16,16 +18,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# WORKAROUND: Avoid seeing the errors:
-# => WARNING: Could not load IOV methods. Check your GSSAPI C library for an update
-# => WARNING: Could not load AEAD methods. Check your GSSAPI C library for an update
-# by setting $VERBOSE=nil momentarily
-if defined?(WinRM).nil?
-  verbose_bk = $VERBOSE
-  $VERBOSE = nil
-  require "winrm"
-  $VERBOSE = verbose_bk
+def silence_warnings
+  old_verbose, $VERBOSE = $VERBOSE, nil
+  yield
+ensure
+  $VERBOSE = old_verbose
 end
+
+silence_warnings { require "winrm" }
 
 require "rbconfig"
 require "uri"
@@ -44,11 +44,11 @@ module Kitchen
     # @author Fletcher Nichol <fnichol@nichol.ca>
     class WinrmFailed < TransportFailed; end
 
-    # Class to help establish WinRM connections, issue remote commands, and
-    # transfer files between a local system and remote node.
+    # A Transport which uses WinRM to execute commands and transfer files.
     #
     # @author Matt Wrock <matt@mattwrock.com>
     # @author Salim Afiune <salim@afiunemaya.com.mx>
+    # @author Fletcher Nichol <fnichol@nichol.ca>
     class Winrm < Kitchen::Transport::Base
 
       default_config :port, 5985
@@ -60,6 +60,7 @@ module Kitchen
       default_config :connection_retry_sleep, 1
       default_config :max_wait_until_ready, 600
 
+      # (see Base#connection)
       def connection(state, &block)
         options = connection_options(config.to_hash.merge(state))
 
@@ -70,10 +71,15 @@ module Kitchen
         end
       end
 
-      # TODO: comment
+      # A Connection instance can be generated and re-generated, given new
+      # connection details such as connection port, hostname, credentials, etc.
+      # This object is responsible for carrying out the actions on the remote
+      # host such as executing commands, transferring files, etc.
+      #
+      # @author Fletcher Nichol <fnichol@nichol.ca>
       class Connection < Kitchen::Transport::Base::Connection
 
-        # (see Base#close)
+        # (see Base::Connection#close)
         def close
           return if @session.nil?
 
@@ -86,7 +92,7 @@ module Kitchen
           @session = nil
         end
 
-        # (see Base#execute)
+        # (see Base::Connection#execute)
         def execute(command)
           logger.debug("[WinRM] #{self} (#{command})")
           exit_code, stderr = execute_with_exit_code(command)
@@ -103,7 +109,7 @@ module Kitchen
           end
         end
 
-        # (see Base#login_command)
+        # (see Base::Connection#login_command)
         def login_command
           case RbConfig::CONFIG["host_os"]
           when /darwin/
@@ -118,7 +124,7 @@ module Kitchen
           end
         end
 
-        # (see Base#wait_until_ready)
+        # (see Base::Connection#wait_until_ready)
         def wait_until_ready
           delay = 3
           session(
@@ -147,22 +153,49 @@ module Kitchen
           HTTPClient::KeepAliveDisconnected
         ].freeze
 
+        # @return [Integer] how many times to retry when failing to execute
+        #   a command or transfer files
+        # @api private
         attr_reader :connection_retries
 
+        # @return [Float] how many seconds to wait before attempting a retry
+        #   when failing to execute a command or transfer files
+        # @api private
         attr_reader :connection_retry_sleep
 
+        # @return [String] the endpoint URL of the remote WinRM host
+        # @api private
         attr_reader :endpoint
 
+        # @return [String] display name for the associated instance
+        # @api private
         attr_reader :instance_name
 
+        # @return [String] local path to the root of the project
+        # @api private
         attr_reader :kitchen_root
 
+        # @return [Integer] how many times to retry when invoking
+        #   `#wait_until_ready` before failing
+        # @api private
         attr_reader :max_wait_until_ready
 
+        # @return [Integer] the TCP port number to use when connection to the
+        #   remote WinRM host
+        # @api private
         attr_reader :rdp_port
 
+        # @return [Symbol] the transport strategy to use when constructing a
+        #   `WinRM::WinRMWebService`
+        # @api private
         attr_reader :winrm_transport
 
+        # Creates a finalizer for this connection which will close the open
+        # remote shell session when the object is garabage collected or on
+        # Ruby VM shutdown.
+        #
+        # @param shell_id [String] the remote shell identifier
+        # @api private
         def add_finalizer(shell_id)
           ObjectSpace.define_finalizer(
             self,
@@ -177,6 +210,12 @@ module Kitchen
           )
         end
 
+        # Writes an RDP document to the local file system.
+        #
+        # @param opts [Hash] file options
+        # @option opts [true,false] :mac whether or not the document is for a
+        #   Mac system
+        # @api private
         def create_rdp_doc(opts = {})
           content = Util.outdent!(<<-RDP)
             full address:s:#{URI.parse(endpoint).host}:#{rdp_port}
@@ -195,6 +234,17 @@ module Kitchen
           end
         end
 
+        # Establish a remote shell session on the remote host.
+        #
+        # @param opts [Hash] retry options
+        # @option opts [Integer] :retries the number of times to retry before
+        #   failing
+        # @option opts [Float] :delay the number of seconds to wait until
+        #   attempting a retry
+        # @option opts [String] :message an optional message to be logged on
+        #   debug (overriding the default) when a rescuable exception is raised
+        # @return [Winrm::CommandExecutor] the command executor session
+        # @api private
         def establish_shell(opts)
           @service = ::WinRM::WinRMWebService.new(
             endpoint, winrm_transport, options)
@@ -209,6 +259,13 @@ module Kitchen
           executor
         end
 
+        # Execute a Powershell script over WinRM and return the command's
+        # exit code and standard error.
+        #
+        # @param command [String] Powershell script to execute
+        # @return [[Integer,String]] an array containing the exit code of the
+        #   script and the standard error stream
+        # @api private
         def execute_with_exit_code(command)
           response = session.run_powershell_script(command) do |stdout, _|
             logger << stdout if stdout
@@ -230,22 +287,31 @@ module Kitchen
           @max_wait_until_ready   = @options.delete(:max_wait_until_ready)
         end
 
+        # Logs formatted standard error output at the warning level.
+        #
+        # @param stderr [String] standard error output
+        # @api private
         def log_stderr_on_warn(stderr)
           error_regexp = /<S S=\"Error\">/
 
-          if stderr.grep(error_regexp).empty?
-            stderr.join.
-              split("\r\n").
-              each { |line| logger.warn(line) }
-          else
-            stderr.join.
+          if error_regexp.match(stderr)
+            stderr.
               split(error_regexp)[1..-2].
               map! { |line| line.sub(/_x000D__x000A_<\/S>/, "").rstrip }.
+              each { |line| logger.warn(line) }
+          else
+            stderr.
+              split("\r\n").
               each { |line| logger.warn(line) }
           end
         end
 
+        # Builds a `LoginCommand` for use by Linux-based platforms.
+        #
         # TODO: determine whether or not `desktop` exists
+        #
+        # @return [LoginCommand] a login command
+        # @api private
         def login_command_for_linux
           args  = %W[ -u #{options[:user]} ]
           args += %W[ -p #{options[:pass]} ] if options.key?(:pass)
@@ -254,26 +320,51 @@ module Kitchen
           LoginCommand.new("rdesktop", args)
         end
 
+        # Builds a `LoginCommand` for use by Mac-based platforms.
+        #
+        # @return [LoginCommand] a login command
+        # @api private
         def login_command_for_mac
           create_rdp_doc(:mac => true)
 
           LoginCommand.new("open", rdp_doc_path)
         end
 
+        # Builds a `LoginCommand` for use by Windows-based platforms.
+        #
+        # @return [LoginCommand] a login command
+        # @api private
         def login_command_for_windows
           create_rdp_doc
 
           LoginCommand.new("mstsc", rdp_doc_path)
         end
 
+        # @return [String] path to the local RDP document
+        # @api private
         def rdp_doc_path
           File.join(kitchen_root, ".kitchen", "#{instance_name}.rdp")
         end
 
+        # Removes any finalizers for this connection.
+        #
+        # @api private
         def remove_finalizer
           ObjectSpace.undefine_finalizer(self)
         end
 
+        # Yields to a block and reties the block if certain rescuable
+        # exceptions are raised.
+        #
+        # @param opts [Hash] retry options
+        # @option opts [Integer] :retries the number of times to retry before
+        #   failing
+        # @option opts [Float] :delay the number of seconds to wait until
+        #   attempting a retry
+        # @option opts [String] :message an optional message to be logged on
+        #   debug (overriding the default) when a rescuable exception is raised
+        # @return [Winrm::CommandExecutor] the command executor session
+        # @api private
         def retryable(opts)
           yield
         rescue *RESCUE_EXCEPTIONS_ON_ESTABLISH => e
@@ -294,11 +385,17 @@ module Kitchen
           end
         end
 
-        def session(connection_options = {})
+        # Establishes a remote shell session, or establishes one when invoked
+        # the first time.
+        #
+        # @param retry_options [Hash] retry options for the initial connection
+        # @return [Winrm::CommandExecutor] the command executor session
+        # @api private
+        def session(retry_options = {})
           @session ||= establish_shell({
             :retries => connection_retries.to_i,
             :delay   => connection_retry_sleep.to_i
-          }.merge(connection_options))
+          }.merge(retry_options))
         end
 
         # String representation of object, reporting its connection details and
@@ -312,6 +409,12 @@ module Kitchen
 
       private
 
+      # Builds the hash of options needed by the Connection object on
+      # construction.
+      #
+      # @param data [Hash] merged configuration and mutable state data
+      # @return [Hash] hash of connection options
+      # @api private
       def connection_options(data)
         opts = {
           :instance_name          => instance.name,
@@ -332,6 +435,12 @@ module Kitchen
         opts
       end
 
+      # Creates a new WinRM Connection instance and save it for potential
+      # future reuse.
+      #
+      # @param options [Hash] conneciton options
+      # @return [Ssh::Connection] a WinRM Connection instance
+      # @api private
       def create_new_connection(options, &block)
         if @connection
           logger.debug("[WinRM] shutting previous connection #{@connection}")
@@ -342,22 +451,38 @@ module Kitchen
         @connection = Kitchen::Transport::Winrm::Connection.new(options, &block)
       end
 
+      # Return the last saved WinRM connection instance.
+      #
+      # @return [Winrm::Connection] a WinRM Connection instance
+      # @api private
       def reuse_connection
         logger.debug("[WinRM] reusing existing connection #{@connection}")
         yield @connection if block_given?
         @connection
       end
 
-      # TODO: comment
+      # An object that can close a remote shell session over WinRM.
+      #
+      # @author Fletcher Nichol <fnichol@nichol.ca>
+      # @api private
       class ShellCloser
 
-        def initialize(*args)
-          @debug = args.shift
-          @shell_id = args.shift
-          @info = args.shift
+        # Constructs a new ShellCloser.
+        #
+        # @param debug [true,false] whether or not debug messages should be
+        #   output
+        # @param shell_id [String] the indentifier for the current open remote
+        #   shell session
+        # @param info [String] a string representation of the connection
+        # @param args [Array] arguments to construct a `WinRM::WinRMWebService`
+        def initialize(debug, shell_id, info, *args)
+          @debug = debug
+          @shell_id = shell_id
+          @info = info
           @args = args
         end
 
+        # Closes the remote shell session.
         def call(*)
           debug("[WinRM] closing remote shell #{@shell_id} on #{@info}")
           ::WinRM::WinRMWebService.new(*@args).close_shell(@shell_id)
@@ -368,8 +493,12 @@ module Kitchen
 
         private
 
-        def debug(msg)
-          $stdout.puts "D      #{msg}" if @debug
+        # Writes a debug message, if debug mode is enabled.
+        #
+        # @param message [String] a message
+        # @api private
+        def debug(message)
+          $stdout.puts "D      #{message}" if @debug
         end
       end
     end
