test-kitchen/test-kitchen

commit 23f4d9450b9080b686058b44d23f8be368dcf2c7

Adding comments and cleaning up code

lib/kitchen/driver/ec2.rb
@@ -94,8 +94,8 @@ module Kitchen
         server.wait_for do
           print '.'
           # Euca instances often report ready before they have an IP
-          ipaddress = Kitchen::Driver::Ec2.hostname(self)
-          ready? && !ipaddress.nil? && ipaddress != '0.0.0.0'
+          hostname = Kitchen::Driver::Ec2.hostname(self)
+          ready? && !hostname.nil? && hostname != '0.0.0.0'
         end
         print '(server ready)'
         state[:hostname] = hostname(server)
@@ -210,30 +210,41 @@ module Kitchen
         end
       end
 
-      def interface_types
+      #
+      # Ordered mapping from config name to Fog name.  Ordered by preference
+      # when looking up hostname.
+      #
+      INTERFACE_TYPES =
         {
           'dns' => 'dns_name',
           'public' => 'public_ip_address',
           'private' => 'private_ip_address'
         }
-      end
 
+      #
+      # Lookup hostname of a provided server using the configured interface.
+      #
       def hostname(server)
-        if config[:interface]
-          interface_type = interface_types.fetch(config[:interface]) do
-            raise Kitchen::UserError, 'Invalid interface'
-          end
-          Kitchen::Driver::Ec2.hostname(server, interface_type)
-        else
-          Kitchen::Driver::Ec2.hostname(server)
-        end
+        Kitchen::Driver::Ec2.hostname(server, config[:interface])
       end
 
+      #
+      # Lookup hostname of provided server.  If interface_type is provided use
+      # that interface to lookup hostname.  Otherwise, try ordered list of
+      # options.
+      #
       def self.hostname(server, interface_type=nil)
         if interface_type
+          INTERFACE_TYPES.fetch(interface_type) do
+            raise Kitchen::UserError, "Invalid interface [#{interface_type}]"
+          end
           server.send(interface_type)
         else
-          server.dns_name || server.public_ip_address || server.private_ip_address
+          potential_hostname = nil
+          INTERFACE_TYPES.values.each do |type|
+            potential_hostname ||= server.send(type)
+          end
+          potential_hostname
         end
       end
 

commit defaa614accac944ff3ae91810a36b35ad18e256

Fix the regression after changes in 23f4d945 which caused every `kitchen test` eventually fail when you provided a value for the `interface` configuration parameter.
The failure message is:

>>>>>> ------Exception-------
>>>>>> Class: Kitchen::ActionFailed
>>>>>> Message: Failed to complete #create action: [undefined method `public' for #<Fog::Compute::AWS::Server:0x000000041bc430>]
>>>>>> ----------------------
>>>>>> Please see .kitchen/logs/kitchen.log for more details
>>>>>> Also try running `kitchen diagnose --all` for configuration

See also:

https://github.com/test-kitchen/kitchen-ec2/commit/23f4d9450b9080b686058b44d23f8be368dcf2c7#diff-c5498dad76244656522b77033980c6a6R238

lib/kitchen/driver/ec2.rb
@@ -247,7 +247,7 @@ module Kitchen
       #
       def self.hostname(server, interface_type=nil)
         if interface_type
-          INTERFACE_TYPES.fetch(interface_type) do
+          interface_type = INTERFACE_TYPES.fetch(interface_type) do
             raise Kitchen::UserError, "Invalid interface [#{interface_type}]"
           end
           server.send(interface_type)