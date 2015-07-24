17589a994cc87309755afbb421110913a0202d3d
tyler-ball <tyleraball@gmail.com>
2015-07-24 15:12:42 -0600

    Updating for Aruba 8

--- features/step_definitions/gem_steps.rb
+++ features/step_definitions/gem_steps.rb
@@ -9,6 +9,7 @@ Given(/^a sandboxed GEM_HOME directory named "(.*?)"$/) do |name|
 
   @aruba_timeout_seconds = 30
 
+  # TODO we need a way to get this path on debug output
   gem_home = Pathname.new(Dir.mktmpdir(name))
   ENV["GEM_HOME"] = gem_home.to_s
   ENV["GEM_PATH"] = [gem_home.to_s, ENV["GEM_PATH"]].join(":")
@@ -17,12 +18,12 @@ end
 
 Then(/^a gem named "(.*?)" is installed with version "(.*?)"$/) do |name, version|
   unbundlerize do
-    run_simple(unescape("gem list #{name} --version #{version} -i"), true, nil)
+    run_simple(Aruba::Platform.unescape("gem list #{name} --version #{version} -i"))
   end
 end
 
 Then(/^a gem named "(.*?)" is installed$/) do |name|
   unbundlerize do
-    run_simple(unescape("gem list #{name} -i"), true, nil)
+    run_simple(Aruba::Platform.unescape("gem list #{name} -i"))
   end
 end
