d74d27353be2de912452dcde16281e139660058e
Yury Tsarev <yury.tsarev@gooddata.com>
2015-08-28 12:15:16 +0200

    Shell verifier: upload test suite during verification stage
    * Create sandbox to upload using :upload_path
    * Copy tests from temp location to :remote_upload_path within an
      instance

--- lib/kitchen/verifier/shell.rb
+++ lib/kitchen/verifier/shell.rb
@@ -53,6 +53,30 @@ module Kitchen
         debug("[#{name}] Verify completed.")
       end
 
+      def create_sandbox
+        super
+        prepare_upload
+      end
+
+      def prepare_upload
+        return unless config[:upload_path]
+        FileUtils.cp_r(config[:upload_path], sandbox_path, :preserve => true)
+      end
+
+      def prepare_command
+        return unless config[:remote_upload_path]
+        commands = []
+        commands << [sudo('cp -r'),
+                     File.join(config[:root_path],
+                               File.basename(config[:upload_path]) + '/*'),
+                     config[:remote_upload_path]
+                    ]
+
+        commands = commands.join(' ')
+        logger.debug commands
+        commands
+      end
+
       # for legacy drivers.
       def run_command
         if config[:remote_exec]
