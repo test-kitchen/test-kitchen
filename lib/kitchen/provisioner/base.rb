8f5b7752b5c6da086a8263879837a8a916a05f87
Alex Lee <alex@mattermark.com>
2015-09-23 12:37:24 -0700

    changing range

--- lib/kitchen/provisioner/base.rb
+++ lib/kitchen/provisioner/base.rb
@@ -59,7 +59,7 @@ module Kitchen
       # @raise [ActionFailed] if the action could not be completed
       def call(state)
         create_sandbox
-        sandbox_dirs = Dir.glob(File.join(sandbox_path, "*")) + Dir.glob(File.join(sandbox_path, ".[1-0a-zA-Z]*"))
+        sandbox_dirs = Dir.glob(File.join(sandbox_path, "*")) + Dir.glob(File.join(sandbox_path, ".[a-zA-Z0-9]*"))
 
         instance.transport.connection(state) do |conn|
           conn.execute(install_command)
