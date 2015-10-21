0592e46e9c1eee51ae2e0eec10ce81a46203bd95
neillturner <neillwturner@gmail.com>
2015-10-21 14:26:37 +0100

    package driver command

--- lib/kitchen/driver/ssh_base.rb
+++ lib/kitchen/driver/ssh_base.rb
@@ -123,6 +123,12 @@ module Kitchen
         raise ClientError, "#{self.class}#destroy must be implemented"
       end
 
+      # Package an instance.
+      #
+      # (see Base#package)
+      def package(state) # rubocop:disable Lint/UnusedMethodArgument
+      end
+
       # (see Base#login_command)
       def login_command(state)
         instance.transport.connection(backcompat_merged_state(state)).
