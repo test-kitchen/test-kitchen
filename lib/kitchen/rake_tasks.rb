76be0e1c057bd2cc7a5d677eb4c652f5876bd81c
Greg Symons <greg.symons@gwfathom.com>
2016-06-29 15:59:51 -0500

    Make RakeTask#config public.
    Given the comment on initialize, I think that a public config accessor was the
    original intent, but when define was made private, the accessor was made private
    as well. I found having a public accessor convenient for "faking" multinode
    support by overriding the rake task to spin up and destroy the dependency
    instances before and after the real task.

--- lib/kitchen/rake_tasks.rb
+++ lib/kitchen/rake_tasks.rb
@@ -44,10 +44,10 @@ module Kitchen
       define
     end
 
-    private
-
     # @return [Config] a Kitchen::Config
     attr_reader :config
+
+    private
 
     # Generates a test Rake task for each instance and one to test all
     # instances in serial.
