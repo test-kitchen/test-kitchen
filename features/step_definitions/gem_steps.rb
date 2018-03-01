bda951c4022c4bae94a7da349738fe1142d7f604
Robb Kidd <robb@thekidds.org>
2018-03-01 16:40:36 -0500

    don't add drivers to a project's Gemfile during init
    Seems this was causing more trouble than it prevented.
    
    Removes the GEM_HOME sandboxing in init feature tests as this only
    existing to provide an environment to test the munging of a Gemfile and
    the subsequent `bundle install`
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- features/step_definitions/gem_steps.rb
+++ features/step_definitions/gem_steps.rb
@@ -3,18 +3,6 @@
 require "tmpdir"
 require "pathname"
 
-Given(/^a sandboxed GEM_HOME directory named "(.*?)"$/) do |name|
-  backup_envvar("GEM_HOME")
-  backup_envvar("GEM_PATH")
-
-  @aruba_timeout_seconds = 30
-
-  gem_home = Pathname.new(Dir.mktmpdir(name))
-  aruba.environment["GEM_HOME"] = gem_home.to_s
-  aruba.environment["GEM_PATH"] = [gem_home.to_s, ENV["GEM_PATH"]].join(":")
-  @cleanup_dirs << gem_home
-end
-
 Then(/^a gem named "(.*?)" is installed with version "(.*?)"$/) do |name, version|
   unbundlerize do
     run_simple(
