9c187352d91c74049c5b76d72067ed50f5d2dd69
Jason Field <4923914+Xorima@users.noreply.github.com>
2020-08-23 21:29:07 +0100

    Update lib/kitchen/cli.rb

--- lib/kitchen/cli.rb
+++ lib/kitchen/cli.rb
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "thor" unless defined?(Thor)
+require "thor"
 
 require_relative "../kitchen"
 require_relative "generator/init"
