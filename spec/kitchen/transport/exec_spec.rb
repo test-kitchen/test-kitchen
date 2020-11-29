e3755a8419dde54e8ec1c27896b13bc49b434524
Lance Albertson <lance@osuosl.org>
2020-11-29 14:19:00 -0800

    Run commands from file on Windows when using exec transport
    Windows limits the length of commands that can be run via powershell which
    breaks the exec transport. To work around the issue, when running on a Windows
    host, the command to a file and then execute the file, and then remove the file.
    
    This resolves #1630.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/kitchen/transport/exec_spec.rb
+++ spec/kitchen/transport/exec_spec.rb
@@ -16,7 +16,12 @@ require_relative "../../spec_helper"
 
 require "kitchen/transport/exec"
 
-describe Kitchen::Transport::Ssh do
+describe Kitchen::Transport::Exec do
+
+  before do
+    RbConfig::CONFIG.stubs(:[]).with("host_os").returns("blah")
+  end
+
   let(:logged_output) { StringIO.new }
   let(:logger)        { Logger.new(logged_output) }
   let(:config)        { {} }
@@ -39,15 +44,62 @@ describe Kitchen::Transport::Ssh do
   end
 
   describe "#connection" do
-    it "returns a Kitchen::Transport::Exec::Connection object" do
-      transport.connection(state).must_be_kind_of Kitchen::Transport::Exec::Connection
+    let(:klass) { Kitchen::Transport::Exec::Connection }
+
+    def self.common_connection_specs
+      before do
+        config[:kitchen_root] = "/i/am/root"
+      end
+
+      it "returns a Kitchen::Transport::Exec::Connection object" do
+        transport.connection(state).must_be_kind_of klass
+      end
+
+      it "sets :instance_name to the instance's name" do
+        klass.expects(:new).with do |hash|
+          hash[:instance_name] == "coolbeans"
+        end
+
+        make_connection
+      end
+
+      it "sets :kitchen_root to the transport's kitchen_root" do
+        klass.expects(:new).with do |hash|
+          hash[:kitchen_root] == "/i/am/root"
+        end
+
+        make_connection
+      end
+
+      describe "called without a block" do
+        def make_connection(s = state) # rubocop:disable Lint/NestedMethodDefinition
+          transport.connection(s)
+        end
+
+        common_connection_specs
+      end
+
+      describe "called with a block" do
+        def make_connection(s = state) # rubocop:disable Lint/NestedMethodDefinition
+          transport.connection(s) do |conn|
+            conn
+          end
+        end
+
+        common_connection_specs
+      end
     end
   end
 end
 
 describe Kitchen::Transport::Exec::Connection do
+  before do
+    RbConfig::CONFIG.stubs(:[]).with("host_os").returns("blah")
+  end
+
   let(:logged_output)   { StringIO.new }
   let(:logger)          { Logger.new(logged_output) }
+  let(:exec_script)     { File.join("/tmp/.kitchen/instance-exec-script.ps1") }
 
   let(:options) do
     { logger: logger }
@@ -67,6 +119,43 @@ describe Kitchen::Transport::Exec::Connection do
       connection.expects(:run_command).never
       connection.execute(nil)
     end
+    describe "for windows-based workstations" do
+      before do
+        RbConfig::CONFIG.stubs(:[]).with("host_os").returns("mingw32")
+        options[:kitchen_root] = "/tmp"
+        options[:instance_name] = "instance"
+      end
+
+      it "runs the command" do
+        stub_file(exec_script, "")
+        connection.expects(:run_command).with("powershell -file \"#{exec_script}\"")
+        connection.execute("do the thing")
+      end
+
+      it "ignores nil" do
+        connection.expects(:run_command).never
+        connection.execute(nil)
+      end
+    end
+  end
+
+  describe "#close" do
+    it "Does not remove exec script file" do
+      FileUtils.expects(:remove).with(exec_script).never
+      connection.close
+    end
+
+    describe "for windows-based workstations" do
+      before do
+        RbConfig::CONFIG.stubs(:[]).with("host_os").returns("mingw32")
+        options[:kitchen_root] = "/tmp"
+        options[:instance_name] = "instance"
+      end
+      it "Removes exec script file" do
+        FileUtils.expects(:remove).with(exec_script)
+        connection.close
+      end
+    end
   end
 
   describe "#upload" do
@@ -75,5 +164,18 @@ describe Kitchen::Transport::Exec::Connection do
       FileUtils.expects(:cp_r).with("/tmp/sandbox/cookbooks", "/tmp/kitchen")
       connection.upload(%w{/tmp/sandbox/cookbooks}, "/tmp/kitchen")
     end
+    it "copies files when $env:temp is set" do
+      ENV["temp"] = "/tmp"
+      FileUtils.expects(:mkdir_p).with("/tmp/kitchen")
+      FileUtils.expects(:cp_r).with("/tmp/sandbox/cookbooks", "/tmp/kitchen")
+      connection.upload(%w{/tmp/sandbox/cookbooks}, "\$env:TEMP\\kitchen")
+    end
+  end
+
+  private
+
+  def stub_file(path, content)
+    FileUtils.mkdir_p(File.dirname(path))
+    File.open(path, "wb") { |f| f.write(content) }
   end
 end
