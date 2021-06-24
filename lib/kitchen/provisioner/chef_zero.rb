d577dcd44d4b87121e2445719a8474fdb0382d41
Pranay Singh <i5singh.pranay@gmail.com>
2021-06-24 21:09:10 +0530

    AFTER CONFLICT RESOLVE adding chef zero class from  breaking API change for anyone who has inherited from ChefZero
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

---
+++ lib/kitchen/provisioner/chef_zero.rb
@@ -0,0 +1,10 @@
+# Deprecated AS PER THE PR - https://github.com/test-kitchen/test-kitchen/pull/1730
+require_relative "chef_infra"
+
+module Kitchen
+  module Provisioner
+    class ChefZero < ChefInfra
+
+    end
+  end
+end
