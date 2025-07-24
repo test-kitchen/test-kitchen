7112f618c5c71b3d2b04e9990fa155ecb170a61b
Tim Smith <tsmith84@gmail.com>
2025-07-24 08:28:55 -0700

    Fix some typos in code
    Simple spellchecking
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- spec/kitchen/data_munger_spec.rb
+++ spec/kitchen/data_munger_spec.rb
@@ -2607,7 +2607,7 @@ module Kitchen # rubocop:disable Metrics/ModuleLength
           )
         end
 
-        it "merges old common busser name to version with exising verifier" do
+        it "merges old common busser name to version with existing verifier" do
           result = DataMunger.new(
             {
               busser: "starship",
