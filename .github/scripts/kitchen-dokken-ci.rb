f7c1f4afaf72a04212010164050fb859ad5026ea
Dan Webb <dan.webb@damacus.io>
2026-06-18 16:28:53 +0100

    ci: guard dokken data container upload

--- .github/scripts/kitchen-dokken-ci.rb
+++ .github/scripts/kitchen-dokken-ci.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 module KitchenDokkenCI
-  def self.patch_helpers
-    return unless defined?(::Dokken::Helpers)
+  def self.patch
+    patch_helpers if defined?(::Dokken::Helpers)
+    patch_provisioner if defined?(::Kitchen::Provisioner::Dokken)
+  end
 
+  def self.patch_helpers
     ::Dokken::Helpers.module_eval do
       def remote_docker_host?
         info = config[:docker_info] || {}
@@ -22,14 +25,52 @@ module KitchenDokkenCI
       end
     end
   end
+
+  def self.patch_provisioner
+    ::Kitchen::Provisioner::Dokken.class_eval do
+      # rubocop:disable Chef/Deprecations/UsesRunCommandHelper
+      def call(state)
+        create_sandbox
+        write_run_command(run_command)
+        instance.transport.connection(state) do |conn|
+          if dokken_data_container_upload?(state)
+            info("Transferring files to #{instance.to_str}")
+            conn.upload(sandbox_dirs, config[:root_path])
+          end
+
+          conn.execute(prepare_command)
+          conn.execute_with_retry(
+            "sh #{config[:root_path]}/run_command",
+            config[:retry_on_exit_code],
+            config[:max_retries],
+            config[:wait_for_retry]
+          )
+        end
+      rescue Kitchen::Transport::TransportFailed => ex
+        raise ActionFailed, ex.message
+      ensure
+        cleanup_dokken_sandbox if config[:clean_dokken_sandbox]
+      end
+      # rubocop:enable Chef/Deprecations/UsesRunCommandHelper
+
+      private
+
+      def dokken_data_container_upload?(state)
+        (remote_docker_host? || running_inside_docker?) &&
+          state.dig(:data_container, :NetworkSettings, :Ports, :"22/tcp")
+      end
+    end
+  end
 end
 
-helper_patch = TracePoint.new(:end) do |trace|
-  next unless defined?(::Dokken::Helpers) && trace.self.equal?(::Dokken::Helpers)
-
-  KitchenDokkenCI.patch_helpers
-  helper_patch.disable
+dokken_patch = TracePoint.new(:end) do |trace|
+  if defined?(::Dokken::Helpers) && trace.self.equal?(::Dokken::Helpers)
+    KitchenDokkenCI.patch
+  elsif defined?(::Kitchen::Provisioner::Dokken) && trace.self.equal?(::Kitchen::Provisioner::Dokken)
+    KitchenDokkenCI.patch
+    dokken_patch.disable
+  end
 end
 
-helper_patch.enable
-KitchenDokkenCI.patch_helpers
+dokken_patch.enable
+KitchenDokkenCI.patch
